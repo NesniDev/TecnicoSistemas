c2922f712138e3bbd988bb9e94bca08ab179ab46
Neider Nieto <nestnidev17@gmail.com>
2025-10-20 17:55:25 -0500

    update from 5 days ago

--- src/pages/api/get-news.ts
+++ src/pages/api/get-news.ts
@@ -5,8 +5,12 @@ import type { APIRoute } from "astro";
 export const GET: APIRoute = async () => {
     
     const API_KEY = 'i2Vrvbmpz55P6g0HEyJ0b7n0BW8mEEJ0VmQeCUgK'
-    
-    const res = await fetch(`https://api.thenewsapi.com/v1/news/all?api_token=${API_KEY}&published_after=2025-10-15&language=es&limit=3&categories=tech&search=computer`)
+    const fechaActual = new Date()
+    const copiaFechaActual = new Date(fechaActual)
+    copiaFechaActual.setDate(fechaActual.getDate() - 3)
+    const fechaMenos5 = copiaFechaActual.toISOString().split('T')[0]
+
+    const res = await fetch(`https://api.thenewsapi.com/v1/news/all?api_token=${API_KEY}&published_after=${fechaMenos5}&language=es&limit=3&categories=tech&search=computer`)
     if(!res.ok){
        return new Response(JSON.stringify({ error: 'Error al consultar la API' }), {
         status: res.status,
