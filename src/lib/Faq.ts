34bd2f96cfac20a4c72fb0f15b2c590902814d3e
Neider Nieto <nestnidev17@gmail.com>
2025-10-20 20:36:51 -0500

    faq blog

---
+++ src/lib/Faq.ts
@@ -0,0 +1,22 @@
+export const faq = [
+    {
+        question: '¿Qué hace un técnico de sistemas en una empresa?',
+        answer: 'Se encarga del mantenimiento de hardware y software, resolución de problemas técnicos, optimización del rendimiento y gestión de redes.'
+    },
+    {
+        question: '¿Cuál es la diferencia entre un técnico de sistemas y un técnico informático?',
+        answer: 'El técnico de sistemas se enfoca en la administración y mantenimiento de redes y servidores, mientras que el técnico informático se centra más en la reparación de equipos y asistencia técnica.'
+    },
+    {
+        question: '¿Qué conocimientos debe tener un técnico de sistemas?',
+        answer: 'Debe dominar redes, hardware, software, seguridad informática y administración de sistemas.'
+    },
+    {
+        question: '¿Cuáles son las habilidades clave?',
+        answer: 'Resolución de problemas, atención al detalle, trabajo bajo presión y comunicación efectiva.'
+    },
+    {
+        question: '¿Cuál es la diferencia entre un técnico de sistemas y un técnico informático?',
+        answer: 'El técnico de sistemas se enfoca en la administración y mantenimiento de redes y servidores, mientras que el técnico informático se centra más en la reparación de equipos y asistencia técnica.'
+    }
+]
