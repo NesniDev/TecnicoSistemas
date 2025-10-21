33aa5741f8ac8c6aa057a09fe8eab337544825a3
Neider Nieto <nestnidev17@gmail.com>
2025-10-21 14:51:01 -0500

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-
 import tailwindcss from '@tailwindcss/vite'
-
+import vercel from '@astrojs/vercel'
 import mdx from '@astrojs/mdx'
-
-import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [mdx()],
-
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: vercel()
 })
