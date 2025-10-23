148e396daea575059f35e73b3c87ff872410bb06
Neider Nieto <nestnidev17@gmail.com>
2025-10-22 23:22:46 -0500

    middleware empty

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -4,30 +4,6 @@ import { defineMiddleware } from "astro:middleware";
 const privateRoutes = ['/loginEstudiante']
 
 export const onRequest = defineMiddleware((context, next) => {
-    
-    const authHeaders = context.request.headers.get('authorization') ?? ''
-
-    if(privateRoutes.includes(context.url.pathname)) return checkLocalAuth(authHeaders, next)
 
     return next();
 })
-
-const checkLocalAuth = ( authHeaders: string, next: MiddlewareNext) => {
-    if(authHeaders){
-        const authValue = authHeaders.split(' ').at(-1) ?? 'user:pass'
-        const decodedValue = atob(authValue).split(':') 
-        console.log(decodedValue)
-        
-        const [user, password] = decodedValue
-        if(user === 'admin' && password==='admin') return next()
-    }
-
-
-
-    return new Response('Autenticacion requerida', {
-        status: 401,
-        headers: {
-            'WWW-Authenticate': 'Basic real="Secure Area"'
-        }
-    })
-}
