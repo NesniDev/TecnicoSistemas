385ca8961fcb699621db30b5f1396d864f575fd5
Neider Nieto <nestnidev17@gmail.com>
2025-10-23 20:28:01 -0500

    environment variables

---
+++ src/env.d.ts
@@ -0,0 +1,10 @@
+/// <reference path="../.astro/actions.d.ts" />
+///    <reference types="astro/client" />
+
+
+declare namespace App {
+    interface Locals {
+
+        isLoggedIn: boolean
+    }
+}
