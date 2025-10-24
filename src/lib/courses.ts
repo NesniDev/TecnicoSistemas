d74773782aaf1c32b4e8e0acbcf09d4aefefb7d1
Neider Nieto <nestnidev17@gmail.com>
2025-10-24 10:50:55 -0500

    information list

---
+++ src/lib/courses.ts
@@ -0,0 +1,35 @@
+export const courses = [
+    {
+        img: '/images/blog.webp',
+        title: 'Introducción a la Programación',
+        description: 'Aprende los fundamentos de la programación desde cero con python. Ideal para principiantes.',
+        name: 'Neider Nieto',
+        modules: '12 Módulos',
+        url: '',
+    },
+    {
+        img: '/images/blog.webp',
+        title: 'Base de Datos',
+        description: 'Domina conceptos de bases de datos y su funcionamiento, crear y manipular tablas, consultar datos, y realizar operaciones de inserción, actualización y eliminación.',
+        name: 'Neider Nieto',
+        modules: '8 Módulos',
+        url: ''
+
+    },
+    {
+        img: '/images/blog.webp',
+        title: 'Desarrollo de Aplicaciones Web',
+        description: 'Aprende a crear aplicaciones web desde cero, incluyendo la estructura en HTML y los estilos CSS.',        
+        name: 'Neider Nieto',
+        modules: '15 Módulos',
+        url: ''
+    },
+    {
+        img: '/images/blog.webp',
+        title: 'Seguridad Informática',
+        description: 'Aprende los conceptos básicos de seguridad informática, incluyendo la autenticación, autorización y protección de datos.',
+        name: 'Neider Nieto',
+        modules: '10 Módulos',
+        url: ''
+    }
+]
---
+++ src/lib/events.ts
@@ -0,0 +1,17 @@
+export const events = [
+    {
+        title: 'Próximos webinars',
+        description: 'Webinars de tecnico en sistemas IFE sobre la IA',
+        date: '20 de Oct de 2025'
+    },
+    {
+        title: 'Fecha limites de tareas',
+        description: 'Fecha limites de tareas para la asignatura de Sistemas de Información',
+        date: '30 de Nov de 2025'
+    },
+    {
+        title: 'Anuncios de cursos',
+        description: 'Anuncios de cursos para la asignatura de Sistemas de Información',
+        date: '15 de Agos de 2025'
+    }
+]
