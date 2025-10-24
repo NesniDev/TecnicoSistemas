f5337a8960ade2ea21a42223d31c3521743a262e
Neider Nieto <nestnidev17@gmail.com>
2025-10-24 15:52:00 -0500

    add items for the courses

--- src/lib/courses.ts
+++ src/lib/courses.ts
@@ -2,34 +2,43 @@ export const courses = [
     {
         img: '/images/blog.webp',
         title: 'Introducción a la Programación',
+        slug: 'introduccion-a-la-programacion',
         description: 'Aprende los fundamentos de la programación desde cero con python. Ideal para principiantes.',
         name: 'Neider Nieto',
         modules: '12 Módulos',
         url: '',
+        learn: ['Comprender los fundamentos de la programación', 'Aprender conceptos básicos de Python', 'Aprender estructuras de control de flujo', 'Aprender estructuras de datos básicas', 'Aprender funciones básicas']
     },
     {
         img: '/images/blog.webp',
         title: 'Base de Datos',
+        slug: 'base-de-datos',
         description: 'Domina conceptos de bases de datos y su funcionamiento, crear y manipular tablas, consultar datos, y realizar operaciones de inserción, actualización y eliminación.',
         name: 'Neider Nieto',
         modules: '8 Módulos',
-        url: ''
-
+        url: '',
+        learn: ['Comprender los fundamentos de la programación', 'Aprender conceptos básicos de Python', 'Aprender estructuras de control de flujo', 'Aprender estructuras de datos básicas', 'Aprender funciones básicas']
     },
     {
         img: '/images/blog.webp',
         title: 'Desarrollo de Aplicaciones Web',
+        slug: 'desarrollo-de-aplicaciones-web',
         description: 'Aprende a crear aplicaciones web desde cero, incluyendo la estructura en HTML y los estilos CSS.',        
         name: 'Neider Nieto',
         modules: '15 Módulos',
-        url: ''
+        url: '',
+                learn: ['Comprender los fundamentos de la programación', 'Aprender conceptos básicos de Python', 'Aprender estructuras de control de flujo', 'Aprender estructuras de datos básicas', 'Aprender funciones básicas']
+
     },
     {
         img: '/images/blog.webp',
         title: 'Seguridad Informática',
+        slug: 'seguridad-informatica',
         description: 'Aprende los conceptos básicos de seguridad informática, incluyendo la autenticación, autorización y protección de datos.',
         name: 'Neider Nieto',
         modules: '10 Módulos',
-        url: ''
+        url: '',
+                learn: ['Comprender los fundamentos de la programación', 'Aprender conceptos básicos de Python', 'Aprender estructuras de control de flujo', 'Aprender estructuras de datos básicas', 'Aprender funciones básicas']
+
     }
 ]
