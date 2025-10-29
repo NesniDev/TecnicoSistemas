4da1afabd69168c82671cfee03c6556f4009df23
Neider Nieto <nestnidev17@gmail.com>
2025-10-28 22:08:32 -0500

    change design for homepage and add tips ts

---
+++ src/lib/tips.ts
@@ -0,0 +1,17 @@
+export const tips = [
+    {
+        title: "Perfecciona tus Fundamentos Técnicos",
+        description: "Domina las bases del hardware, los sistemas operativos y las redes. Comprender cómo funcionan los componentes físicos y lógicos es esencial para ofrecer soluciones efectivas y confiables.",
+        image: ""
+    },
+    {
+        title: "Fortalece tu Red Profesional",
+        description: "Participa en comunidades tecnológicas, foros y proyectos colaborativos. Interactuar con otros técnicos y profesionales del sector ampliará tus oportunidades y conocimientos.",
+        image: ""
+    },
+    {
+        title: "Piensa como un Técnico en Soluciones",
+        description: "Más allá de reparar o configurar equipos, aprende a analizar problemas, optimizar procesos y aplicar pensamiento lógico. La resolución eficiente de incidencias es tu mejor carta de presentación.",
+        image: ""
+    }
+];
