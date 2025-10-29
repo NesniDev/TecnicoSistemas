56468a9d241e7af90968ceed4d3b65f7b1d6adc0
Neider Nieto <nestnidev17@gmail.com>
2025-10-28 22:06:58 -0500

    add pagination with server-action

--- src/actions/courses/get-courses-by-page.ts
+++ src/actions/courses/get-courses-by-page.ts
@@ -1,29 +1,45 @@
-import { actions, defineAction } from 'astro:actions'
+import { defineAction } from 'astro:actions'
 import { z } from 'astro/zod'
 import { courses } from '@lib/courses'
 
-export const getCourseByPage = defineAction( {
-    accept: 'json',
-    input: z.object({
-        page: z.number().optional().default(1),
-        limit: z.number().optional().default(4),
-    }),
-    handler: async ({ page, limit }) => {
-        
-        page = page <= 0 ? 1 : page
-        const totalPage = Math.ceil(courses.length / limit)
+export const getCourseByPage = defineAction({
+  accept: 'json',
+  input: z.object({
+    page: z.number().optional().default(1),
+    limit: z.number().optional().default(4), 
+    search: z.string().optional(),
+  }),
+  handler: async ({ page, limit, search }) => {
+    let filteredCourses = courses
 
-        if(page > totalPage) {
-            return {
-                courses: [],
-                totalPage,
-            }
-        }
+    if (search) {
+      const normalizeText = (text: string) =>
+        text
+          .normalize('NFD')
+          .replace(/[\u0300-\u036f]/g, '')
+          .toLowerCase();
 
-        const coursesByPage = courses.slice((page - 1) * limit, (page * limit ) )
-        return {
-            courses: coursesByPage,
-            totalPage,
-        }
+      const searchTerm = normalizeText(search);
+      filteredCourses = courses.filter((course) =>
+        normalizeText(course.title).includes(searchTerm)
+      );
     }
+
+    page = page <= 0 ? 1 : page
+    const totalPage = Math.ceil(filteredCourses.length / limit)
+
+    if (page > totalPage && totalPage > 0) {
+      page = totalPage
+    }
+
+    const coursesByPage = filteredCourses.slice(
+      (page - 1) * limit,
+      page * limit
+    )
+
+    return {
+      courses: coursesByPage,
+      totalPage,
+    }
+  },
 })
