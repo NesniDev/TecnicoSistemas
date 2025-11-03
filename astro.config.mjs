1f17002ed5773e6825fbe3fa874a3b0ee3bc81c3
Neider Nieto <nestnidev17@gmail.com>
2025-11-02 22:21:24 -0500

    config astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,14 @@ import { defineConfig } from 'astro/config'
 import vercel from '@astrojs/vercel/serverless'
 import mdx from '@astrojs/mdx'
 
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-<<<<<<< HEAD
   integrations: [mdx()],
-  adapter: vercel({}),
+  adapter: vercel(),
   output: 'server',
   vite: {
     plugins: [tailwindcss()]
   }
-=======
-  integrations: [mdx(), tailwind()],
-  adapter: vercel(),
-  output: 'server'
->>>>>>> cf921d32a6d6aa9506a0ecbba2c5bcb974808791
 })
