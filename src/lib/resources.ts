f9461a10134be62b4f459458bfbe6eaa8fe7896d
Neider Nieto <nestnidev17@gmail.com>
2025-11-12 17:12:55 -0500

    add new resources

--- src/lib/resources.ts
+++ src/lib/resources.ts
@@ -105,5 +105,168 @@ export const resources = [
         view: false,
         mode: 'evaluación',
         url: 'https://docs.google.com/uc?export=download&id=1j9NmKMH2vvEHnAbGbtmlMjuxvP2_BD7K'
-    }
+    },
+    //Bases de datos 
+    {
+        type: 'rar',
+        title: 'Pagos en Linea',
+        description: 'Ejercicio para importar datos desde un excel a access y manipularlos.',
+        view: true,
+        mode: 'ejercicio',
+        url: 'https://drive.google.com/uc?export=download&id=1QGywexDbvdm6Wwrfwn-rxez4QQL8DzPv'
+    },
+    {
+        type: 'access',
+        title: 'Filtros, Orden y Consultas',
+        description: 'Ejercicio para aprender a usar filtros, orden y consultas avanzadas en access.',
+        view: true,
+        mode: 'ejercicio',
+        url: 'https://drive.google.com/uc?export=download&id=1VVpRCY4cEhWx75-0IIlSF0YM3MeZd08T'
+    },
+    {
+        type: 'access',
+        title: 'Evaluación de Bases de datos',
+        description: 'Evaluación final de Bases de datos que permita evaluar el conocimiento adquirido en el curso.',
+        view: false,
+        mode: 'evaluación',
+        url: 'https://drive.google.com/uc?export=download&id=1I_C-2Xy6owqMkjuKO1rKkBu5zbBdviXz'
+    },
+    {
+        type: 'pdf',
+        title: 'Casos para crear bases de datos',
+        description: 'Crear diferentes bases de datos de acuerdo a los ejercicios planteados.',
+        view: true,
+        mode: 'trabajo',
+        url: 'https://drive.google.com/uc?export=download&id=1PRsRrMaIVyJK-cY8x39fOjF1W_ykVC2c'
+    },
+    {
+        type: 'txt',
+        title: 'Relaciones entre tablas ejercicios',
+        description: 'Ejercicios para aprender a crear relaciones sencillas entre tablas en access.',
+        view: true,
+        mode: 'Ejercicio',
+        url: 'https://drive.google.com/uc?export=download&id=1eulfRscE2Sjv2rESehhv3LbTxK37b212'
+    },
+    {
+        type: 'excel',
+        title: 'Excel para la explicación de las relaciones',
+        description: 'Excel para la explicación básica de como se realizan las relaciones entre tablas en access.',
+        view: true,
+        mode: 'Ejercicio',
+        url: 'https://docs.google.com/uc?export=download&id=16IeMc39yAWxpU6Q6diGEAccUVHnhlAQ4'
+    },
+    {
+        type: 'pdf',
+        title: 'Trabajo sobre la creación de base de datos con Access',
+        description: 'Trabajo para la creación de tablas, consultas, relaciones y formularios en Access.',
+        view: true,
+        mode: 'trabajo',
+        url: 'https://drive.google.com/uc?export=download&id=1-deYFGapDnwfG78CgGdTG0tsC7-mxE9R'
+    },
+    {
+        type: 'pdf',
+        title: 'Trabajo de refuerzo Biblioteca',
+        description: 'Trabajo de refuerzo sobre una base de datos de una biblioteca, incluye tablas, consultas, relaciones y formularios.',
+        view: false,
+        mode: 'refuerzo',
+        url: 'https://drive.google.com/uc?export=download&id=11BZusJnevM7go04DYA7bEsS8JJwKElMv'
+    },
+    {
+        type: 'pdf',
+        title: 'Trabajo de refuerzo Hospital',
+        description: 'Trabajo de refuerzo sobre una base de datos de un hospital, incluye tablas, consultas, relaciones y formularios.',
+        view: false,
+        mode: 'refuerzo',
+        url: 'https://drive.google.com/uc?export=download&id=11D42bjQNw39qSbMGgx1twuWUPu4KFwPs'
+    },
+    {
+        type: 'pdf',
+        title: 'Trabajo de refuerzo Tienda',
+        description: 'Trabajo de refuerzo sobre una base de datos de una tienda, incluye tablas, consultas, relaciones y formularios.',
+        view: false,
+        mode: 'refuerzo',
+        url: 'https://drive.google.com/uc?export=download&id=1ZKGOcMd3TnOuRTUQhnz7swU6d5XQiFjq'
+    },
+    //fundamentos de programación
+    {
+        type: 'pdf',
+        title: 'Algoritmos',
+        description: 'Guía para aprender a resolver problemas con algoritmos.',
+        view: true,
+        mode: 'guía',
+        url: 'https://drive.google.com/uc?export=download&id=1IDi_TU7H1ERbYQaekaZDl3q92LGNaqlI'
+    },
+    {
+        type: 'pdf',
+        title: 'Fundamentos de programación',
+        description: 'Guía para aprender sobre la historia y los fundamentos de programación.',
+        view: true,
+        mode: 'guía',
+        url: 'https://drive.google.com/uc?export=download&id=1jGg3XpMo8mDZXpC19aWpMAb0QEGlWMFx'
+    },
+    {
+        type: 'pdf',
+        title: 'Taller en DFD',
+        description: 'Taller para aprender a resolver problemas con algoritmos en DFD.',
+        view: true,
+        mode: 'trabajo',
+        url: 'https://drive.google.com/uc?export=download&id=1zQ2FKVHz09I-3viX5SIdgbgAP1u_bsj_'
+    },
+    {
+        type: 'pdf',
+        title: 'Taller en PSEINT',
+        description: 'Taller para aprender a resolver problemas con algoritmos en PSEINT.',
+        view: true,
+        mode: 'trabajo',
+        url: 'https://drive.google.com/uc?export=download&id=19mEMFJUuwmuNoSGkREpOCPzzD3NsLqIn'
+    },
+    {
+        type: 'pdf',
+        title: 'Instalación de Python y Sublime Text',
+        description: 'Guía para instalar Python y Sublime Text.',
+        view: true,
+        mode: 'guía',
+        url: 'https://drive.google.com/uc?export=download&id=1ff6DKWelLffaNlEhRFJfJjxpJFdawUcU'
+    },
+    {
+        type: 'pdf',
+        title: 'Calculadora por consola.',
+        description: 'Creación de una Calculadora por consola en python con manejo de variables, condicionales y bucles.',
+        view: false,
+        mode: 'guía - Trabajo',
+        url: 'https://drive.google.com/uc?export=download&id=1h2KGust0IW1HFfr_nmqOUe_lOXtHPI61'
+    },
+    {
+        type: 'pdf',
+        title: 'Calculadora por Interfaz.',
+        description: 'Creación de una Calculadora por Interfaz en python con manejo Tkinter',
+        view: false,
+        mode: 'guía - Trabajo',
+        url: 'https://drive.google.com/uc?export=download&id=1pWctY0Lk65bu0XpoAL1Tg6VQeUfxCgle'
+    },
+    //Diseño Web
+    {
+        type: 'img',
+        title: 'Página Web - IFE COLOMBIA',
+        description: 'Creación de una Página web en HTML y CSS que permita mostrar la información de cada uno de los estudiantes.',
+        view: true,
+        mode: 'Ejercicio',
+        url: 'https://drive.google.com/uc?export=download&id=13K4Huor1HFIS8LRkiZk_YL39RLAAXwmH'
+    },
+    {
+        type: 'pdf',
+        title: 'Página Web - IFE COLOMBIA',
+        description: 'Guía para la creación de una página web en HTML y CSS.',
+        view: false,
+        mode: 'trabajo',
+        url: 'https://drive.google.com/uc?export=download&id=1t3E76gCwRPM1gBRjmVYFcP6SvwPiep9R'
+    },
+    {
+        type: 'pdf',
+        title: 'Página Web - Biografia estudiantes',
+        description: 'Cree una página web en HTML y CSS que permita mostrar la información personal de cada uno de los estudiantes.',
+        view: true,
+        mode: 'trabajo',
+        url: 'https://drive.google.com/uc?export=download&id=1ZOf6XKM0KR3_BhgrYco3VW-6Y63buU-Y'
+    },
 ]
