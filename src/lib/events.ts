c632cff575383a09ee4f54a798fb903f2588430d
Neider Nieto <nestnidev17@gmail.com>
2025-11-12 12:16:11 -0500

    Icon update if the event date is reached

--- src/lib/events.ts
+++ src/lib/events.ts
@@ -3,18 +3,21 @@ export const events = [
         title: 'Grados de fin de año',
         description: 'Graduación de fin de año de los estudiantes de IFE COLOMBIA',
         date: '22 de Nov de 2025',
+        date_end: '15/12/2026',
         time: '10:00 AM - 04:00 PM | Aula Julio Florez'
     },
     {
         title: 'Fecha limites de Matriculas',
         description: 'Fecha limites de matriculas para los estudiantes de IFE COLOMBIA',
         date: '22 de Ene de 2026',
+        date_end: '22/01/2026',
         time: '8:00 AM - 04:00 PM | Instituto IFE Colombia'
     },
     {
         title: 'Anuncios de cursos disponibles',
         description: 'Anuncios de cursos nuevos que estarán disponibles en IFE Colombia para 2026',
         date: '10 de Ene de 2026',
+        date_end: '10/01/2026',
         time: '8:30 AM - 04:00 PM | Instituto IFE Colombia'
     }
 ]
