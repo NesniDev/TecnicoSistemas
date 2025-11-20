8c297521a9b0ea1998e3ae1974514a695c6bbfb0
Neider Nieto <nestnidev17@gmail.com>
2025-11-20 12:04:45 -0500

    Add Seo and siteMap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,19 @@
 import { defineConfig } from 'astro/config'
 import vercel from '@astrojs/vercel/serverless'
 import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
 import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  site: 'https://tecnico-sistemas.vercel.app',
+  integrations: [
+    mdx(),
+    sitemap({
+      filter: (page) => page !== 'https://tecnico-sistemas.vercel.app/'
+    })
+  ],
   adapter: vercel(),
   output: 'server',
   vite: {
